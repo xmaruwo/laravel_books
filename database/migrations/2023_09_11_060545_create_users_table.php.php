ceaf67a7ba5d02eea4b0be0e5cf71f48249c6378
xmaruwo <xmaruwo@gmail.com>
2023-09-12 16:43:32 +0900

    独自に作成したusersテーブルに合わせてDB関連ファイル自動生成

---
+++ database/migrations/2023_09_11_060545_create_users_table.php.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 255)->unique();
+            $table->string('nickname');
+            $table->string('password')->comment('パスワードhash値');
+            $table->boolean('visibility')->default(1)->comment('0:非公開、1:公開');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
